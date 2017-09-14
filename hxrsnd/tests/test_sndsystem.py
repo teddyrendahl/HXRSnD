f976125844aacdd69f203559306efa805de11490
apra93 <dd.apra93@gmail.com>
2017-09-14 16:09:05 -0700

    TST: Added tests for the motor movability in the towers.

--- hxrsnd/tests/test_sndsystem.py
+++ hxrsnd/tests/test_sndsystem.py
@@ -24,6 +24,8 @@ from pcdsdevices.sim.pv import using_fake_epics_pv
 ##########
 from hxrsnd import sndsystem
 from .conftest import get_classes_in_module
+from hxrsnd.sndsystem import DelayTower, ChannelCutTower
+from hxrsnd.exceptions import MotorDisabled, MotorFaulted
 
 logger = logging.getLogger(__name__)
 
@@ -35,3 +37,40 @@ def test_devices_instantiate_and_run_ophyd_functions(dev):
     assert(isinstance(device.describe(), OrderedDict))
     assert(isinstance(device.describe_configuration(), OrderedDict))
     assert(isinstance(device.read_configuration(), OrderedDict))
+
+@using_fake_epics_pv
+def test_DelayTower_does_not_move_if_motors_not_ready():
+    tower = DelayTower("TEST")
+    tower.disable()
+    time.sleep(.5)
+    tower.tth.limits = (-100, 100)
+    tower.th1.limits = (-100, 100)
+    tower.th2.limits = (-100, 100)
+
+    tower.tth.user_setpoint.check_value = lambda x: None
+    tower.th1.user_setpoint.check_value = lambda x: None
+    tower.th2.user_setpoint.check_value = lambda x: None    
+
+    with pytest.raises(MotorDisabled):
+        tower.energy = 10
+    tower.enable()
+    tower.tth.axis_fault._read_pv._value = True
+    with pytest.raises(MotorFaulted):
+        tower.energy = 10
+
+@using_fake_epics_pv
+def test_ChannelCutTower_does_not_move_if_motors_not_ready():
+    tower = ChannelCutTower("TEST")
+    tower.disable()
+    time.sleep(.5)
+    tower.th.limits = (-100, 100)
+    tower.th.user_setpoint.check_value = lambda x: None
+
+    with pytest.raises(MotorDisabled):
+        tower.energy = 10
+    tower.enable()
+    tower.th.axis_fault._read_pv._value = True
+    with pytest.raises(MotorFaulted):
+        tower.energy = 10
+
+        
