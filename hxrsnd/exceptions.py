d57384e77786379f3dc3090ae9b436d6c34c493d
apra93 <dd.apra93@gmail.com>
2017-09-22 13:51:13 -0700

    ENH: Added a new exception for bad N2 pressures.

--- hxrsnd/exceptions.py
+++ hxrsnd/exceptions.py
@@ -38,13 +38,23 @@ class MotorDisabled(SnDException):
 
 class MotorFaulted(SnDException):
     """
-    Exception raised when an action requiring the motor not be faulted is requested.
+    Exception raised when an action requiring the motor not be faulted is 
+    requested.
     """
     pass
 
 
 class MotorError(SnDException):
     """
-    Exception raised when an action requiring the motor not have an error is requested.
+    Exception raised when an action requiring the motor not have an error is 
+    requested.
+    """
+    pass
+
+
+class BadN2Pressure(SnDException):
+    """
+    Exception raised when an action requiring the N2 pressure be good is 
+    requested with a bad pressure.
     """
     pass
