fbb71aca4352a179bd59e18742d059402dfe0b2b
Zachary Lentz <zlentz@slac.stanford.edu>
2017-12-18 15:43:56 -0800

    ENH: Split up snd_devices to be importable in scripts

--- bin/run_snd.py
+++ bin/run_snd.py
@@ -1,34 +1,17 @@
 """
 HXRSnD IPython Shell
 """
-############
-# Standard #
-############
 import os
 import socket
 import logging
 from imp import reload
 from pathlib import Path
 import warnings
-###############
-# Third Party #
-###############
-from bluesky import RunEngine
-from bluesky.preprocessors import run_wrapper
 
-########
-# SLAC #
-########
-from pcdsdevices.daq import make_daq_run_engine
+# from bluesky.preprocessors import run_wrapper
 
-##########
-# Module #
-##########
-from hxrsnd.sndmotor import SamMotor
-from hxrsnd.sequencer import SeqBase
+# from hxrsnd.plans.scans import linear_scan
 from hxrsnd.utils import setup_logging
-from hxrsnd.sndsystem import SplitAndDelay
-from hxrsnd.plans.scans import linear_scan
 
 # Ignore python warnings
 warnings.filterwarnings('ignore')
@@ -38,21 +21,10 @@ setup_logging()
 logger = logging.getLogger("hxrsnd")
 
 try:
-    # Base PV
-    pv_base = "XCS:SND"
-    # Instantiate the whole system
-    snd = SplitAndDelay(pv_base)
-    # Create a RunEngine
-    RE = RunEngine({})
-    RE_daq = make_daq_run_engine(snd.daq)
-    # Additional Devices
-    seq = SeqBase("ECS:SYS0:4", desc="Sequencer Channel 4")
-    sam_x = SamMotor("XCS:USR:MMN:01", name="sam_x")
-    sam_y = SamMotor("XCS:USR:MMN:02", name="sam_y")
+    from snd_devices import *
     # Success
     logger.debug("Successfully created SplitAndDelay class on '{0}'".format(
         socket.gethostname()))
-
 except Exception as e:
     logger.error("Failed to create SplitAndDelay class on '{0}'. Got error: "
                  "{1}".format(socket.gethostname(), e))
---
+++ bin/snd_devices.py
@@ -0,0 +1,23 @@
+# from bluesky import RunEngine
+
+# from pcdsdevices.daq import make_daq_run_engine
+
+from hxrsnd.sndmotor import SamMotor
+from hxrsnd.sequencer import SeqBase
+from hxrsnd.sndsystem import SplitAndDelay
+
+# Base PV
+pv_base = "XCS:SND"
+
+# Instantiate the whole system
+snd = SplitAndDelay(pv_base)
+daq = snd.daq
+
+# Create a RunEngine
+# RE = RunEngine({})
+# RE_daq = make_daq_run_engine(snd.daq)
+
+# Additional Devices
+seq = SeqBase("ECS:SYS0:4", desc="Sequencer Channel 4")
+sam_x = SamMotor("XCS:USR:MMN:01", name="sam_x")
+sam_y = SamMotor("XCS:USR:MMN:02", name="sam_y")
