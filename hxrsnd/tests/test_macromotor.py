7ce94bc4b508157a5c657919e7b4754e846caed7
apra93 <dd.apra93@gmail.com>
2018-02-06 15:16:50 -0800

    TST: Also undoing change to macromotor tests.

--- hxrsnd/tests/test_macromotor.py
+++ hxrsnd/tests/test_macromotor.py
@@ -22,18 +22,15 @@ from pcdsdevices.sim.pv import using_fake_epics_pv
 # Module #
 ##########
 from .conftest import get_classes_in_module, fake_device
-from hxrsnd.macromotor import MacroBase
-from hxrsnd.sndsystem import SplitAndDelay
+from hxrsnd import macromotor
 
 logger = logging.getLogger(__name__)
 
 @using_fake_epics_pv
-def test_macromotors_instantiate_and_run_ophyd_functions():
-    snd = fake_device(SplitAndDelay)
-    for comp_name in snd.component_names:
-        component = getattr(snd, comp_name)
-        if issubclass(type(component), MacroBase):
-            assert(isinstance(component.read(), dict))
-            assert(isinstance(component.describe(), dict))
-            assert(isinstance(component.describe_configuration(), dict))
-            assert(isinstance(component.read_configuration(), dict))
+@pytest.mark.parametrize("dev", get_classes_in_module(macromotor, Device))
+def test_devices_instantiate_and_run_ophyd_functions(dev):
+    device = fake_device(dev)
+    assert(isinstance(device.read(), dict))
+    assert(isinstance(device.describe(), dict))
+    assert(isinstance(device.describe_configuration(), dict))
+    assert(isinstance(device.read_configuration(), dict))
