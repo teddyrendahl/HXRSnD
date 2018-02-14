2232c7453f7abf1ee3606a54143102f592d6a532
apra93 <dd.apra93@gmail.com>
2018-02-13 18:49:46 -0800

    TST: Adding the basic test for SndDevice

--- hxrsnd/tests/test_snddevice.py
+++ hxrsnd/tests/test_snddevice.py
@@ -9,13 +9,13 @@ from ophyd.device import Device
 from ophyd.tests.conftest import using_fake_epics_pv
 
 from .conftest import get_classes_in_module, fake_device
-from hxrsnd import sndmotor
+from hxrsnd import snddevice
 
 logger = logging.getLogger(__name__)
 
 @using_fake_epics_pv
-@pytest.mark.parametrize("dev", get_classes_in_module(sndmotor, Device))
-def test_sndmotor_devices_instantiate_and_run_ophyd_functions(dev):
+@pytest.mark.parametrize("dev", get_classes_in_module(snddevice, Device))
+def test_sndevice_devices_instantiate_and_run_ophyd_functions(dev):
     device = fake_device(dev)
     assert(isinstance(device.read(), OrderedDict))
     assert(isinstance(device.describe(), OrderedDict))
